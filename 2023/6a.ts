be94db2d1f8931c06596fe9b283d2061f3eb7b6d
Ekke Vasli <ekke@ukumae.ee>
2023-12-06 06:48:27 +0000

    6: Mathing for fun and no profit

--- 2023/6a.ts
+++ 2023/6a.ts
@@ -2,7 +2,6 @@ import { fileAsLines } from "./lib/file.ts";
 import { ints } from "./lib/string.ts";
 
 const lines = fileAsLines('6.in');
-
 const times = ints(lines.shift()!);
 const dists = ints(lines.shift()!);
 
@@ -14,11 +13,28 @@ for (const race of times.keys()) {
     const target = dists[race];
     const time = times[race];
 
-    for (let speed = 1; speed < time; speed++) {
-        const rem = time - speed;
-        if (rem * speed > target)
-            results[race]++;
-    }
+    /*
+     Later optimization, math, with help from mr gpt:
+      Quadratic ax^2 + bx + c = 0 => x = (-b += sq(b^2 - 4ac))/2a
+      (time-speed) * speed = target
+      time*speed - speed*speed = target
+      time*speed = target + speed * speed
+      speed*speed - time*speed + target = 0
+      [chatgpt, help solve this equation for 'speed']
+      speed = (time +- Math.sqrt(time*time - 4*target )) / 2;
+     */
+    const discriminant =  Math.sqrt(time*time - 4*target);
+    const start = Math.ceil((time - discriminant) / 2);
+    const end   = Math.ceil((time + discriminant) / 2);
+    results[race] = end-start;
+
+    // Original submission brute force:
+    // for (let speed = 1; speed < time; speed++) {
+    //     const rem = time - speed;
+    //     if (rem * speed > target)
+    //         results[race]++;
+    // }
 }
 
 console.log({results, res: results.reduce((p, v)=>p*v, 1)})
+// { results: [ 42, 54, 12, 23 ], res: 625968 }
--- 2023/6b.ts
+++ 2023/6b.ts
@@ -13,12 +13,47 @@ for (const race of times.keys()) {
     const target = dists[race];
     const time = times[race];
 
-    for (let speed = 1; speed < time; speed++) {
-        if (!(speed%1000000)) { console.log(speed)}
-        const rem = time - speed;
-        if (rem * speed > target)
-            results[race]++;
-    }
+    /*
+     Later optimization, math, with help from mr gpt:
+      Quadratic ax^2 + bx + c = 0 => x = (-b += sq(b^2 - 4ac))/2a
+      (time-speed) * speed = target
+      time*speed - speed*speed = target
+      time*speed = target + speed * speed
+      speed*speed - time*speed + target = 0
+      [chatgpt, help solve this equation for 'speed']
+      speed = (time +- Math.sqrt(time*time - 4*target )) / 2;
+     */
+    const discriminant =  Math.sqrt(time*time - 4*target);
+    const start = Math.ceil((time - discriminant) / 2);
+    const end   = Math.ceil((time + discriminant) / 2);
+    results[race] = end-start;
+
+    // Earlier later optimization, half brute force, not-quite-binary-search from both ends
+    // let [start, end] = [0,0];
+    // for (let speed = 1; speed < time; speed++) {
+    //     if ((time-speed) * speed > target) {
+    //         start = speed;
+    //         break;
+    //     }
+    // }
+
+    // for (let speed = time; speed > 0; speed--) {
+    //     if ((time-speed) * speed > target) {
+    //         end = speed;
+    //         break;
+    //     }
+    // }
+    // results[race] = end-start+1;
+
+    // Original submission brute force:
+    // for (let speed = 1; speed < time; speed++) {
+    //     if (0 == speed%MILLION)
+    //         console.log(speed);
+    //     const rem = time - speed;
+    //     if (rem * speed > target)
+    //         results[race]++;
+    // }
 }
 
 console.log({results, res: results.reduce((p, v)=>p*v, 1)})
+// { results: [ 43663323 ], res: 43663323 }
