33b679f267c74332d62632d5c0c67ea3098e83dc
Ekke Vasli <ekke@ukumae.ee>
2024-12-25 05:32:03 +0000

    fmt

--- 2024/25a.ts
+++ 2024/25a.ts
@@ -1,15 +1,15 @@
 import { File, globalProtos } from "./lib/index.ts";
-import { rotateMatrix } from "./lib/matrix.ts";
+import { matrixGet, rotateMatrix } from "./lib/matrix.ts";
 import { matrixFromString } from "./lib/matrix.ts";
 globalProtos();
 
-const [keys, locks] = File.string("25.in").split('\n\n')
-    .map(s => matrixFromString(s))
-    .split(m => m[0][0] === '.')
-    .map(g => g.map(m => rotateMatrix(m).map(c => c.count('#'))));
+const [keys, locks] = File.string("25.in").split("\n\n")
+  .map(matrixFromString)
+  .split((m) => matrixGet(m, [0, 0]) === ".")
+  .map((grp) => grp.map((m) => rotateMatrix(m).map((c) => c.count("#"))));
 
-const res = keys.flatMap(key => locks.map(lock => 
-    key.every((keyPin, pin) => keyPin + lock[pin] <= 7)
-)).count(true);
+const res = keys.flatMap((key) =>
+  locks.map((lock) => key.every((keyPin, pin) => keyPin + lock[pin] <= 7))
+).count(true);
 
 console.log({ res }); // 3065
